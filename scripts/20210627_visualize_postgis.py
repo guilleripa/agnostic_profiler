b17b53468a6f928c59ec9e065176fdcaa9a439a2
guilleripa <guilleripa21@gmail.com>
2021-07-13 21:40:46 -0300

    Update requirements.txt and test script.

--- scripts/20210627_visualize_postgis.py
+++ scripts/20210627_visualize_postgis.py
@@ -17,8 +17,8 @@ df.plot()
 DIJKSTRA_QUERY = """
 SELECT * FROM pgr_dijkstra('
                 SELECT gid as id,
-                         source::integer,
-                         target::integer,
+                         source_osm::bigint as source,
+                         target_osm::bigint as source,
                          cost::double precision as cost
                         FROM ways',
                 {}, {}, false);
@@ -27,8 +27,8 @@ SELECT * FROM pgr_dijkstra('
 ASTAR_QUERY = """
 SELECT * FROM pgr_astar('
     SELECT gid::integer AS id,
-         source::integer,
-         target::integer,
+         source_osm::bigint as source,
+         target_osm::bigint as target,
          length::double precision AS cost,
          x1, y1, x2, y2
         FROM ways',
@@ -41,5 +41,6 @@ from pgsql_pathfinder import PgSQLPathFinder
 
 path_finder = PgSQLPathFinder()
 # %%
-path_finder.get_path(1, 300)
+path_finder.get_path(287752964, 259017838)
+
 # %%
